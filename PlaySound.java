e172118ed09ece99cd1165c08d3246596c8d0728
AnkitAti <ankmonuati@gmail.com>
2014-03-18 11:22:44 +0530

    Create PlaySound.java
    Plays the sound!

---
+++ PlaySound.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class PlaySound {
+	
+	public static void play(String arg)
+	{
+		try {
+	         // Open an audio input stream.
+			URL url = PlaySound.class.getResource(arg);
+	         AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+	         // Get a sound clip resource.
+	         Clip clip = AudioSystem.getClip();
+	         // Open audio clip and load samples from the audio input stream.
+	         clip.open(audioIn);
+	         clip.start();
+	      } catch (UnsupportedAudioFileException e) {
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } catch (LineUnavailableException e) {
+	         e.printStackTrace();
+	      } catch(NullPointerException e){
+			 e.printStackTrace();
+		  }
+	}
+}
